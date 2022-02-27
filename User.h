956dec77c01afeb1989940f04a1b57e5ce96a06a
Tanner <tannercdolby@gmail.com>
2022-02-27 01:23:12 -0800

    cleanup

--- User.h
+++ User.h
@@ -10,30 +10,10 @@ using namespace std;
 #include "Book.h"
 
 class User {
-private:
-	// String   | int| int             | vector<Book>
-	// Username | id | # of checkedOut | List of checked out (3 at most!)
-	// --------- ---- ----------------- --------------------------------
-	// gnaare   | 1  | 3               | [Book1, Book2, Book3]
-	// root     | 2  | 1               | [Book1]
-	void logAction(ofstream& ofs, LibraryCard card) {
-		ofs.open("./library-log.txt", ios::app);
-		string s = username + " | " + to_string(card.id) + " | " + to_string(card.booksCheckedOut) + " | ";
-		if (!checkedOutBooks.empty()) {
-			for (int i=0; i < checkedOutBooks.size(); i++) {
-				s += "[" + checkedOutBooks[i].title + ", ";
-			}
-			s += "]";
-		}
-		ofs << s << endl;
-		ofs.close();
-	}
 public:
 	string username;
 	LibraryCard libraryCard;
 	vector<Book> checkedOutBooks;
-
-	int lastCreatedCardId;
 
 	User() {
 		ofstream ofs;
@@ -42,8 +22,6 @@ public:
 		username = "default-user";
 		libraryCard = card;
 		checkedOutBooks = books;
-		lastCreatedCardId = card.id;
-//		logAction(ofs, libraryCard);
 	}
 
 	User(string name) {
@@ -53,12 +31,10 @@ public:
 		username = name;
 		libraryCard = card;
 		checkedOutBooks = books;
-//		logAction(ofs, libraryCard);
 	}
 
-	// username <LibraryCard> or username <NULL>
 	void printUser() {
-		cout << username;
+		cout << username << ": ";
 		libraryCard.printCard();
 	}
 
